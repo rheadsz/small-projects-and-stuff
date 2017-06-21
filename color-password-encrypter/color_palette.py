fe0b35f195d27309bf7dac03f03722096d474d2e
Joel Benjamin <joelbenjamin093@gmail.com>
2017-06-21 20:06:52 +0530

    Update color_palette.py

--- color-password-encrypter/color_palette.py
+++ color-password-encrypter/color_palette.py
@@ -1,4 +1,24 @@
 
+"""
+
+Color palette for GUIs
+    Copyright (C) 2017  Joel Benjamin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import random
 from tkinter import *
 from colors import COLORS
@@ -114,10 +134,3 @@ class ColorPalette():
 		self.child_win.update()
 		print(self.filter_colornames(self.current_color))
 		return self.filter_colornames(self.current_color)
-	#	except Exception as e:
-	#		print(e)
-	
-
-#root = Tk()
-#win = ColorPalette(root)
-#root.mainloop()
